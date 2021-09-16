4d67da1489dec326f08525c4ccd00113417ee5ad
velikura <mrgroove91@gmail.com>
2021-09-16 11:57:21 +0300

    Первый коммит

---
+++ src/IntroTask3Digits.java
@@ -0,0 +1,10 @@
+class IntroTask3Digits {
+    public static void main(String[] args) {
+        int x = 99; //x - данное число
+        if (x>=100&&x<= 999||x>=-999&&x<=-100) {
+            System.out.println("ровно три цифры");
+        } else {
+            System.out.println("не три цифры");
+        }
+    }
+}
