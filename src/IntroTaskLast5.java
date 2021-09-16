9d221cc38c5b9dce95dc9ec11eae572c5f3ed679
velikura <mrgroove91@gmail.com>
2021-09-16 12:50:59 +0300

    Второе задание

---
+++ src/IntroTaskLast5.java
@@ -0,0 +1,10 @@
+class IntroTaskLast5 {
+    public static void main(String[] args) {
+        int x = 5; //x - данное число
+        if (x%5==0&&x%10!=0){
+            System.out.println("на конце 5");
+        } else {
+            System.out.println("на конце не 5");
+        }
+    }
+}
