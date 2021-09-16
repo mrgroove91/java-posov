d6b1e25fe1fe45eefe3d642d2510f52f41ad1a9c
velikura <mrgroove91@gmail.com>
2021-09-16 17:45:10 +0300

    Третья задача

---
+++ src/IntroTaskLeap.java
@@ -0,0 +1,16 @@
+class IntroTaskLeap {
+    public static void main(String[] args){
+        int x = 1000; //x - данный год
+        if (x%4==0){
+            if (!(x%100==0 && x%400!=0)){
+                System.out.println("год високосный");
+            }
+            else{
+                System.out.println("год не високосный");
+            }
+        }
+        else{
+            System.out.println("год не високосный");
+        }
+    }
+}
